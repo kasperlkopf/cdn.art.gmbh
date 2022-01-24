4a8ad02e11af6d7546f5a617dbf732c51236a0c9
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-01-24 14:14:37 +0100

    Update

---
+++ tampermonkey/block-alerts.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Block alerts in PEET
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Manuel
+// @match        https://mf.artgmbh.com/Logistik/CreateDelivery
+// @grant        none
+// ==/UserScript==
+
+;(function() {
+  'use strict'
+
+  window.alert = (msg) => {
+    const now = Date.now()
+    const alert = `<div id="alert-${now}" class="loading-screen">
+                    <div class="p-2 px-3" style="background: #45aa0f; border-radius: 3px; color: #fff;">${msg}</div>
+                  </div>`
+
+    document.body.innerHTML += alert
+    setTimeout(() => document.querySelector('[id^="alert-"]').remove(), 4000)
+  }
+
+})()
---
+++ tampermonkey/peet-commissioning-auto-refresh.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         PEET Commissioning Auto Refresh
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Manuel
+// @match        https://mf.artgmbh.com/ART/ie50/AddOn/ppit/PEETLogistics/Commissioning/PEETCommissioning.aspx*
+// @grant        none
+// ==/UserScript==
+
+window.alert = (msg) => console.log(msg)
+
+;(function() {
+  'use strict'
+
+  let checkBtn
+  let checkBtnTxt
+  let timeout
+  let counter
+
+  let active = false
+
+  document.querySelector('.sidebar__nav > ul > li:nth-child(2)').addEventListener('click', () => {
+    console.log('nav item click')
+
+    checkBtn = document.querySelector('.content__heading .btn-warning')
+    checkBtnTxt = checkBtn.innerText
+
+    if (!active) {
+      active = true
+
+      document.addEventListener('mousemove', () => {
+        reset()
+        timeout = setTimeout(start, 30000)
+      })
+    }
+  })
+
+  function start() {
+    console.log('start')
+
+    counter = 60
+    tick()
+  }
+
+  function tick() {
+    checkBtn.innerHTML = checkBtnTxt + '<span class="badge ml-1" style="background: #009ee0; color: #fff;">' + counter + '</span>'
+
+    if (counter) {
+      counter--
+      timeout = setTimeout(tick, 1000)
+    } else {
+      console.log('refresh')
+
+      checkBtn.click()
+      reset()
+      timeout = setTimeout(start, 20000)
+    }
+  }
+
+  function reset() {
+    clearTimeout(timeout)
+    checkBtn.innerHTML = checkBtnTxt
+  }
+})()

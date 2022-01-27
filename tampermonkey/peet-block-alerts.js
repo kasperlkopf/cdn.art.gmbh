37f7759968e44f642c085e8a945b4c33875333b1
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-01-27 15:19:02 +0100

    Update peet-block-alerts.js

--- tampermonkey/peet-block-alerts.js
+++ tampermonkey/peet-block-alerts.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Block alerts in PEET
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      20220127-1
 // @author       Manuel
 // @match        https://mf.artgmbh.com/Logistik/CreateDelivery
 // @grant        none
@@ -11,13 +11,13 @@
   'use strict'
 
   window.alert = (msg) => {
-    const now = Date.now()
+    const now = Date.now();
     const alert = `<div id="alert-${now}" class="loading-screen">
                     <div class="p-2 px-3" style="background: #45aa0f; border-radius: 3px; color: #fff;">${msg}</div>
-                  </div>`
+                  </div>`;
 
-    document.body.innerHTML += alert
-    setTimeout(() => document.querySelector('[id^="alert-"]').remove(), 4000)
+    document.body.innerHTML += alert;
+    setTimeout(() => document.querySelector('[id^="alert-"]').remove(), 4000);
   }
 
-})()
+})();

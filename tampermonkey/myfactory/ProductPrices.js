f92db393473d1fb87a239ef09eee0909966e0cf1
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-03-11 20:29:46 +0100

    Create ProductPrices.js

---
+++ tampermonkey/myfactory/ProductPrices.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         ProductPrices
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically opens product price list
+// @author       Manuel
+// @match        https://mf.artgmbh.com/ART_PP/ie50/Base/Products/Prices/ProductPrices.aspx?EditName=BaseProducts*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=artgmbh.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Click the button
+  document.querySelector('#cmdShowScale_R0').click();
+
+  // Clear standard price
+  document.querySelector('#txtPrice_R0').value = '';
+})();
